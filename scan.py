ae52ea143b53f355ef13484858f17caee0fc0b26
Basil Schneider <basil.schneider@cern.ch>
2016-01-26 18:22:44 -0600

    Adapt run_all to run over new SLHA files

--- scan.py
+++ scan.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     MY_SCAN = MassScan()
     MY_SCAN.set_threshold(1.)
     L_COORDINATES = [50*i for i in range(5, 8)]
-    M_3 = [250*i for i in range(-7, 3)]
-    MU_EWSB = [100*i for i in range(1, 10)]
-    MY_SCAN.l_m_3 = [-1100, -1000]
-    MY_SCAN.l_mu_ewsb = [500, 600]
+    M_3 = [250*i for i in range(0, 7)]
+    MU_EWSB = [250*i for i in range(0, 7)]
+    MY_SCAN.l_m_3 = [500, 1000]
+    MY_SCAN.l_mu_ewsb = [500, 1000]
     PLOTS = MY_SCAN.do_scan()
     PLOTS.set_rootfile('histos.root')
-    PLOTS.set_directory('test08')
-    PLOTS.set_star(-1071.46632, 534.761347)
+    PLOTS.set_directory("test09/suspect2_lha_2_1000")
+    #PLOTS.set_star(-1071.46632, 534.761347)
     PLOTS.plot()

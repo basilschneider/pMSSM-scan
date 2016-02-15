dbe7adaa9a02afdb9aa4a832ac3d5626a0f7125b
Basil Schneider <basil.schneider@cern.ch>
2016-02-15 08:29:24 -0600

    Changes to example scan and merge scripts

--- scan.py
+++ scan.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     MY_SCAN = MassScan()
     MY_SCAN.set_threshold(1.)
     L_COORDINATES = [50*i for i in range(5, 8)]
-    M_3 = [250*i for i in range(0, 7)]
-    MU_EWSB = [250*i for i in range(0, 7)]
-    MY_SCAN.l_m_3 = [500, 1000]
-    MY_SCAN.l_mu_ewsb = [500, 1000]
+    M_3 = [250*i for i in range(1, 4)]
+    MU_EWSB = [250*i for i in range(1, 3)]
+    MY_SCAN.l_m_3 = M_3
+    MY_SCAN.l_mu_ewsb = MU_EWSB
     PLOTS = MY_SCAN.do_scan()
     PLOTS.set_rootfile('histos.root')
-    PLOTS.set_directory("test09/suspect2_lha_2_1000")
+    PLOTS.set_directory("test12")
     #PLOTS.set_star(-1071.46632, 534.761347)
     PLOTS.plot()

41215b5c00a2dddacdee56c63137532acf85a6d1
Basil Schneider <basil.schneider@cern.ch>
2017-01-05 11:44:03 -0600

    First try of a final higgsino grid

--- scan.py
+++ scan.py
@@ -5,19 +5,19 @@
 from MassScan import MassScan
 
 if __name__ == "__main__":
-    X = [200*i for i in range(1, 11)]
-    Y = [10*i for i in range(8, 19)]
+    X = [100*i for i in range(2, 9)]
+    Y = [10*i for i in range(9, 17)]
 
     MY_SCAN = MassScan()
-    MY_SCAN.set_threshold(.001)
+    MY_SCAN.set_threshold(1.)
     MY_SCAN.l_prmtr_x = X
     MY_SCAN.l_prmtr_y = Y
     MY_SCAN.set_parameter_add_scale_x(2, 2.)
 
     MY_SCAN.set_parameter(1, 23)
 
-    output = 'output76_higgsino'
-    name = 'higgsino-scan-mum1m2'
+    output = 'output80_higgsino'
+    name = 'final-try01'
 
     PLOTS = MY_SCAN.do_scan()
     PLOTS.set_rootfile('{}/{}.root'.format(output, name))
